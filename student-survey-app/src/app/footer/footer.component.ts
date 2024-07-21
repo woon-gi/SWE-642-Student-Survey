efc1259a67e8995f6d5a8880b13ae27e9ae88915
jimmyt333 <46909890+jimmyt333@users.noreply.github.com>
2024-07-21 10:12:08 -0400

    Header, Footer, Welcoem comments

--- student-survey-app/src/app/footer/footer.component.ts
+++ student-survey-app/src/app/footer/footer.component.ts
@@ -1,3 +1,16 @@
+/**
+ * Name: Woon-Gi Hong
+ * G#: G01032192
+ * Name: Jimmy Tran
+ * G#: G01130635
+ * Course-Section: SWE642-001
+ * Assignment: #3
+ * Description: Serves the purpose of maintaining UI 
+ * conventions and creates space from the bottom of the 
+ * page. This component ensures a consistent look and feel 
+ * by persisting across all the pages in the app.
+ **/
+
 import { Component } from '@angular/core';
 
 @Component({
